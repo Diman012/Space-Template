2c9921059d521321e621c8252113add23dd5f545
oDqnger <103481200+Diman012@users.noreply.github.com>
2022-09-23 20:09:24 +0100

    Initial commit

---
+++ Space/index.js
@@ -0,0 +1,7 @@
+$("p").click(function () {
+    $("p").text = "hi"
+})
+
+
+// rn im trying to figure out how to do jquery. which is essential just like javascript, but just simplified.
+// the thing with jquery is that there are custom animations, so im trying to do that :)
